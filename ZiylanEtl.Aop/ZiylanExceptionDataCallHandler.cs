2c87ba9ed6503b160998cf88cf03a0c13776bcfd
ilkerhalil <ilkerhalil@gmail.com>
2016-01-28 09:48:31 +0200

    ZiylanExceptionDataCallHandler eklendi.

---
+++ ZiylanEtl.Aop/ZiylanExceptionDataCallHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Practices.Unity.InterceptionExtension;
+
+namespace ZiylanEtl.Aop
+{
+    public class ZiylanExceptionDataCallHandler : ICallHandler
+    {
+        public IMethodReturn Invoke(IMethodInvocation input, GetNextHandlerDelegate getNext)
+        {
+            if (input == null)
+                throw new ArgumentNullException("input");
+            if (getNext == null)
+                throw new ArgumentNullException("getNext");
+            var methodReturn = getNext()(input, getNext);
+            if (methodReturn.Exception == null) return methodReturn;
+            for (var index = 0; index < input.Arguments.Count; ++index)
+                methodReturn.Exception.Data.Add(input.Arguments.GetParameterInfo(index).Name, input.Arguments[index]);
+            //("Method Parameter :",getNext.Method.par);
+            return methodReturn;
+        }
+
+        public int Order { get; set; }
+    }
+}
