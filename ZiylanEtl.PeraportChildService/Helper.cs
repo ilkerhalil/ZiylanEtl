1a5f35abaae1e8616a11e2d142001f4c07cf501c
ilkerhalil <ilkerhalil@gmail.com>
2016-02-01 11:35:31 +0200

    DtoSet'i implement edildi.

---
+++ ZiylanEtl.PeraportChildService/Helper.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using AutoMapper;
+using ZiylanEtl.Abstraction.Helper;
+using ZiylanEtl.PeraportChildService.Dto;
+using ZiylanEtl.PeraportChildService.ServiceProxy;
+
+namespace ZiylanEtl.PeraportChildService
+{
+    internal static class Helper
+    {
+        public static ZrtEntPeraportRequest CreateRequest(string filter, string erdat, string dateFormat = "yyyy-MM-dd")
+        {
+            var zrnEntPeraPort = new ZrtEntPeraport
+            {
+                Erdat =
+                    string.IsNullOrWhiteSpace(erdat)
+                        ? string.Format(DateTime.Now.AddDays(-1).ToString(dateFormat))
+                        : erdat,
+                C1 = "",
+                C2 = "",
+                C3 = "",
+                C4 = "",
+                C5 = "",
+                C6 = "",
+                C7 = "",
+                C8 = "",
+                C9 = "",
+                C10 = "",
+                C11 = "",
+                C12 = "",
+                C13 = "",
+                C14 = "",
+                C15 = "",
+                C16 = "",
+                GtZinventHrk = new[] { new ZinventHrk(), },
+                GtZinventAsorti = new[] { new ZinventAsorti(), },
+                GtZinventFyt = new[] { new ZinventFyt(), },
+                GtZinventMlz = new[] { new ZinventMlz2(), },
+                GtZinventStok = new[] { new ZinventStok(), },
+                GtZinventStokA = new[] { new ZinventStokA(), },
+                GtZinventTes = new[] { new ZinventTes(), },
+                GtZinventTrn = new[] { new ZinventTrn(), },
+                GtZinventUy = new[] { new ZinventUy(), },
+                GtT6wst = new[] { new T6wst(), },
+                GtT134t = new[] { new T134t(), },
+                GtLfa1 = new[] { new ZentLfa1(), },
+                GtT001 = new[] { new ZentT001(), },
+                GtT023t = new[] { new T023t(), },
+                GtWrfBrandsT = new[] { new WrfBrandsT(), },
+                GtZinventSas = new[] { new ZinventSas(), }
+            };
+
+            var property = zrnEntPeraPort.GetType().GetProperties().Single(w => w.Name == filter);
+            property.SetValue(zrnEntPeraPort, "X");
+            var request = new ZrtEntPeraportRequest
+            {
+                ZrtEntPeraport = zrnEntPeraPort
+            };
+            return request;
+        }
+        public static IMapper CreateMapper()
+        {
+            var mapperConfiguration = new MapperConfiguration(con =>
+            {
+                con.CreateMap<T023t, T023tDto>();
+                con.CreateMap<T134t, T134tDto>();
+                con.CreateMap<T6wst, T6wstDto>();
+                con.CreateMap<WrfBrandsT, WrfBrandsTDto>();
+                con.CreateMap<ZentLfa1, ZentLfa1Dto>();
+                con.CreateMap<ZentT001, ZentT001Dto>();
+                con.CreateMap<ZinventAsorti, ZinventAsortiDto>();
+                con.CreateMap<ZinventFyt, ZinventFytDto>();
+                con.CreateMap<ZinventHrk, ZinventHrkDto>();
+                con.CreateMap<ZinventMlz2, ZinventMlzDto>();
+                con.CreateMap<ZinventSas, ZinventSasDto>();
+                con.CreateMap<ZinventStok, ZinventStokDto>();
+                con.CreateMap<ZinventStokA, ZinventStokADto>();
+                con.CreateMap<ZinventTes, ZinventTesDto>();
+                con.CreateMap<ZinventTrn, ZinventTrnDto>();
+                con.CreateMap<ZinventUy, ZinventUyDto>();
+            });
+            return mapperConfiguration.CreateMapper();
+        }
+        public static void CreateLog(DateTime baslamaZamani, string content, DateTime bitisZamani, TimeSpan toplamZaman)
+        {
+            var builder = new StringBuilder();
+            builder.AppendLine("<html><head><meta charset=\"utf-8\" /></head><body><div>");
+            builder.AppendFormat("<b>Başlama Zamanı {0}</b>", baslamaZamani);
+            builder.AppendLine("<ul>");
+            builder.AppendLine(content);
+            builder.AppendLine("</ul>");
+            builder.AppendLine("</div>");
+            builder.AppendFormat("<b>Bitiş Zamanı {0}</b>", bitisZamani.ToString("F"));
+            builder.AppendFormat("<b>Geçen Zaman {0}</b>", toplamZaman.ToString("G"));
+            builder.AppendLine("</body></html>");
+            File.WriteAllText(Path.GetRandomFileName() + ".txt", builder.ToString());
+        }
+        public static string SerializeNonEmptyCollections(ZrtEntPeraportResponse1 response)
+        {
+            var arrayProperties = response.ZrtEntPeraportResponse.GetType().GetProperties().Where(w =>
+w.PropertyType.IsArray);
+            var st = arrayProperties.ToDictionary(s => s.Name, info => (info.GetValue(response.ZrtEntPeraportResponse) as Array).Length);
+            var dolukoleksiyonlar = st.ToFormattedString("<li>{0} : {1} \n</li>", Environment.NewLine);
+            return dolukoleksiyonlar;
+        }
+    }
+}
--- ZiylanEtl.PeraportChildService/PeraPortSapWsClient.cs
+++ ZiylanEtl.PeraportChildService/PeraPortSapWsClient.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Security;
 using System.Text;
 using AutoMapper;
@@ -17,13 +18,17 @@ namespace ZiylanEtl.PeraportChildService
     /* Peraport ETL Webservice Client */
     public class PeraPortSapWsClient : BaseEtlChildService
     {
+        private DtoSet DtoSet { get; }
 
         #region Fields
 
-        private StringBuilder _builder;
         private readonly IDataAccess _dataAccess;
         private readonly ZRT_ENT_PERAPORTClient _zRtEntPeraportClient;
-        private readonly IMapper _mapperInit;
+        private readonly string[] _filters =
+            {
+                "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11","C12", "C13", "C14", "C15", "C16"
+            };
+
 
         #endregion
 
@@ -43,56 +48,8 @@ namespace ZiylanEtl.PeraportChildService
 
         #endregion
 
-        public override void StartService()
-        {
 
-            var toplamZaman = Stopwatch.StartNew();
-            ZrtEntPeraportResponse1 response = null;
-
-            var filters = new[]
-            {
-                "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11","C12", "C13", "C14", "C15", "C16"
-            };
-
-            CreateLog(DateTime.Now);
-
-            foreach (var filter in filters)
-            {
-                var startNew = Stopwatch.StartNew();
-                var exceptionMessage = string.Empty;
-                try
-                {
-                    var request = CreateRequest(filter);
-                    response = _zRtEntPeraportClient.ZrtEntPeraport(request);
-                }
-                catch (Exception ex)
-                {
-                    exceptionMessage = $"<li>Exception Message {ex}\n</li>";
-                }
-
-                var dosyaAdi =
-                    $"Filtre : {filter} \n Test Zamanı : {DateTime.Now.ToLongDateString() + ":" + DateTime.Now.ToLongTimeString()}";
-
-                var gecenZaman = startNew.Elapsed;
-                startNew.Stop();
-                var dolukoleksiyonlar = string.Empty;
-                if (string.IsNullOrWhiteSpace(exceptionMessage))
-                {
-                    dolukoleksiyonlar = SerializeNonEmptyCollections(response);
-                }
-
-                var logContent = string.Concat(dosyaAdi, Environment.NewLine, exceptionMessage, Environment.NewLine,
-                    gecenZaman, Environment.NewLine, dolukoleksiyonlar);
-
-                AppendLog(logContent);
-            }
-
-            var toplamGecenZaman = toplamZaman.Elapsed;
-            SaveLog(DateTime.Now, toplamGecenZaman);
-            toplamZaman.Stop();
-            //Datayı map edeceğiz
-            //Datayı insert edeceğiz
-        }
+        #region PrivateMethods
 
         private void InitWebServiceClient()
         {
@@ -100,118 +57,79 @@ namespace ZiylanEtl.PeraportChildService
             _zRtEntPeraportClient.ClientCredentials.UserName.Password = Password;
         }
 
-       
         private void ValidateServiceParameter()
         {
             if (Username.IsNullAndWhiteSpace()) throw new SecurityException("Username boş olamaz");
             if (Password.IsNullAndWhiteSpace()) throw new SecurityException("Password boş olamaz");
         }
 
-        private static string SerializeNonEmptyCollections(ZrtEntPeraportResponse1 response)
+        #endregion
+
+
+        public PeraPortSapWsClient(IDataAccess dataAccess)
         {
-            var arrayProperties = response.ZrtEntPeraportResponse.GetType().GetProperties().Where(w => w.PropertyType.IsArray);
-            var st = arrayProperties.ToDictionary(s => s.Name, info => (info.GetValue(response.ZrtEntPeraportResponse) as Array).Length);
-            var dolukoleksiyonlar = st.ToFormattedString("<li>{0} : {1} \n</li>", Environment.NewLine);
-            return dolukoleksiyonlar;
+            _dataAccess = dataAccess;
+            InitWebServiceClient();
+            DtoSet = new DtoSet();
         }
 
-        private ZrtEntPeraportRequest CreateRequest(string filter)
+        public override void StartService()
         {
-            var zrnEntPeraPort = new ZrtEntPeraport
+            var baslamaZamani = DateTime.Now;
+            var toplamZaman = Stopwatch.StartNew();
+            ValidateServiceParameter();
+            ZrtEntPeraportResponse1 response = null;
+
+            var logContent = string.Empty;
+            foreach (var filter in _filters)
             {
-                Erdat =
-                    string.IsNullOrWhiteSpace(this.Erdat)
-                        ? string.Format(DateTime.Now.AddDays(-1).ToString("yyyy-MM-dd"))
-                        : this.Erdat,
-                C1 = "",
-                C2 = "",
-                C3 = "",
-                C4 = "",
-                C5 = "",
-                C6 = "",
-                C7 = "",
-                C8 = "",
-                C9 = "",
-                C10 = "",
-                C11 = "",
-                C12 = "",
-                C13 = "",
-                C14 = "",
-                C15 = "",
-                C16 = "",
-                GtZinventHrk = new[] { new ZinventHrk(), },
-                GtZinventAsorti = new[] { new ZinventAsorti(), },
-                GtZinventFyt = new[] { new ZinventFyt(), },
-                GtZinventMlz = new[] { new ZinventMlz2(), },
-                GtZinventStok = new[] { new ZinventStok(), },
-                GtZinventStokA = new[] { new ZinventStokA(), },
-                GtZinventTes = new[] { new ZinventTes(), },
-                GtZinventTrn = new[] { new ZinventTrn(), },
-                GtZinventUy = new[] { new ZinventUy(), },
-                GtT6wst = new[] { new T6wst(), },
-                GtT134t = new[] { new T134t(), },
-                GtLfa1 = new[] { new ZentLfa1(), },
-                GtT001 = new[] { new ZentT001(), },
-                GtT023t = new[] { new T023t(), },
-                GtWrfBrandsT = new[] { new WrfBrandsT(), },
-                GtZinventSas = new[] { new ZinventSas(), }
-            };
+                var startNew = Stopwatch.StartNew();
+                var exceptionMessage = string.Empty;
 
-            var property = zrnEntPeraPort.GetType().GetProperties().Single(w => w.Name == filter);
-            property.SetValue(zrnEntPeraPort, "X");
-            var request = new ZrtEntPeraportRequest
-            {
-                ZrtEntPeraport = zrnEntPeraPort
-            };
-            return request;
+                var request = Helper.CreateRequest(filter, Erdat);
+                response = _zRtEntPeraportClient.ZrtEntPeraport(request);
+
+
+                var parameterName = $"Filtre : {filter} \n Test Zamanı : {DateTime.Now.ToLongDateString() + ":" + DateTime.Now.ToLongTimeString()}";
+                var gecenZaman = startNew.Elapsed;
+                startNew.Stop();
+
+
+                //info => (info.GetValue(response.ZrtEntPeraportResponse) as Array
+                var dolukoleksiyonlar = Helper.SerializeNonEmptyCollections(response);
+
+                var enumerable = response.ZrtEntPeraportResponse.GetType()
+                    .GetProperties().Where(w => w.PropertyType.IsArray && w.GetValue(response.ZrtEntPeraportResponse) != null && (w.GetValue(response.ZrtEntPeraportResponse) as Array).Length > 1)
+                    .Select(s => s.GetValue(response.ZrtEntPeraportResponse)).Single();
+                AddDtoSet(enumerable);
+                logContent += string.Concat(parameterName, Environment.NewLine, exceptionMessage, Environment.NewLine, gecenZaman, Environment.NewLine, dolukoleksiyonlar);
+            }
+            var bitisZamani = DateTime.Now;
+            var toplamGecenZaman = toplamZaman.Elapsed;
+            toplamZaman.Stop();
+            Helper.CreateLog(baslamaZamani, logContent, bitisZamani, toplamGecenZaman);
+    
+
+            //Datayı insert edeceğiz
+
         }
 
-        private void CreateLog(DateTime baslamaZamani)
+        private void AddDtoSet(object o)
         {
-            _builder = new StringBuilder();
-            _builder.AppendLine("<html><head><meta charset=\"utf-8\" /></head><body><div>");
-            _builder.AppendFormat("<b>Başlama Zamanı {0}</b>", baslamaZamani);
-            _builder.AppendLine("<ul>");
+            var mapper = Helper.CreateMapper();
+            var targetType = mapper.ConfigurationProvider.GetAllTypeMaps().Single(s => s.SourceType == o.GetType().GetElementType()).DestinationType;
+            var listType = typeof(List<>);
+            var constructedListType = listType.MakeGenericType(targetType);
+            var tr = mapper.Map(o, o.GetType(), constructedListType);
+            DtoSet.GetType().GetProperties().Single(s => s.PropertyType == tr.GetType()).SetValue(tr, DtoSet);
         }
 
-        private void AppendLog(string content)
-        {
-            _builder.AppendLine(content);
-        }
 
-        private void SaveLog(DateTime bitisZamani, TimeSpan toplamZaman)
-        {
-            _builder.AppendLine("</ul>");
-            _builder.AppendLine("</div>");
-            _builder.AppendFormat("<b>Bitiş Zamanı {0}</b>", bitisZamani.ToString("F"));
-            _builder.AppendFormat("<b>Geçen Zaman {0}</b>", toplamZaman.ToString("G"));
-            _builder.AppendLine("</body></html>");
-            File.WriteAllText(Path.GetRandomFileName() + ".txt", _builder.ToString());
-        }
 
-        private static IMapper MapperInit()
-        {
-            var mapperConfiguration = new MapperConfiguration(con =>
-            {
-                con.CreateMap<T023t, T023tDto>();
-                con.CreateMap<T134t, T134tDto>();
-                con.CreateMap<T6wst, T6wstDto>();
-                con.CreateMap<WrfBrandsT, WrfBrandsTDto>();
-                con.CreateMap<ZentLfa1, ZentLfa1Dto>();
-                con.CreateMap<ZentT001, ZentT001Dto>();
-                con.CreateMap<ZinventAsorti, ZinventAsortiDto>();
-                con.CreateMap<ZinventFyt, ZinventFytDto>();
-                con.CreateMap<ZinventHrk, ZinventHrkDto>();
-                con.CreateMap<ZinventMlz2, ZinventMlzDto>();
-                con.CreateMap<ZinventSas, ZinventSasDto>();
-                con.CreateMap<ZinventStok, ZinventStokDto>();
-                con.CreateMap<ZinventStokA, ZinventStokADto>();
-                con.CreateMap<ZinventTes, ZinventTesDto>();
-                con.CreateMap<ZinventTrn, ZinventTrnDto>();
-                con.CreateMap<ZinventUy, ZinventUyDto>();
-            });
-            return mapperConfiguration.CreateMapper();
-        }
+
+
+
+
 
 
     }
